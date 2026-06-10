0ff2abe012167b696d2a4d6721b79adedf86386d
Armel Ngomade <armelngomade@yahoo.fr>
2026-06-10 18:43:17 +0100

    fix cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'https://inscriptionestlcunv-ebolowa.vercel.app/', 'https://sigge.vercel.app/'],
+    'allowed_origins' => ['https://inscriptionestlcunv-ebolowa.vercel.app/', 'https://sigge.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
