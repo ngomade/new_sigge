630a5f32d4f87c0f52cfb4dcea2a9d984540cc13
Armel Ngomade <armelngomade@yahoo.fr>
2026-06-16 12:17:25 +0100

    fix cors o2

--- frontend/src/concours/api/index.js
+++ frontend/src/concours/api/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://backend.estlc-unv-ebolowa.com/api/concours' : 'http://localhost:8000/api/concours';
+const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://backend.progsa2026.com/api/concours' : 'http://localhost:8000/api/concours';
 
 const getToken = () => localStorage.getItem('token');
 
