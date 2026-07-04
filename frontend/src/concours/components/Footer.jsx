34a4329a2d171fafafb2bdff8693410fa51df032
Corei913th <bogningfredy972@gmail.com>
2026-07-04 20:50:53 +0100

    chore: modification footer

--- frontend/src/concours/components/Footer.jsx
+++ frontend/src/concours/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Footer() {
   return (
     <div className='bg-white w-full p-2 flex text-gray-500 justify-between items-center'>
-        <Link to={'/'} className='text-xl font-bold'>ESTLC</Link>
+        <Link to={'/'} className='text-xl font-bold'>ESTLC &copy; { new Date().getFullYear() }</Link>
         <p>Copyright &copy; { new Date().getFullYear() }</p>
     </div>
   )
